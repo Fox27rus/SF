8697e4b0727d4f13eaa2831467b59269bd52c03d
Fox27rus <rikoo1011@gmail.com>
2021-08-17 18:33:50 +1000

    Init Skillfactory

--- 21.py
+++ 21.py
@@ -1,4 +1,7 @@
 M = ['-' for i in range(1, 10)]  # Создаем поле для игры
+print('''Приветствую, дорогие игроки в крестики - нолики!
+Правила игры просты: Вам надо выбирать клетку поля и писать его номер от 1 до 9,
+где "1" находится в координате x1z1, "2" в y1z1 и т.д.''')
 
 
 def playing_field():   # Функция для красивого поля
@@ -10,7 +13,8 @@ def playing_field():   # Функция для красивого поля
 
 playing_field()
 
-while True:  # Цикл для игры
+
+def player_1():
     print('ходит игрок 1')
     a = input('Введите число от 1 до 9 : ')
     if a not in [str(i) for i in range(1, 10)]:  # Проверка, что ввели цифру от 1 до 9
@@ -30,7 +34,32 @@ while True:  # Цикл для игры
             if 'x' not in M[int(a) - 1] and '0' not in M[int(a) - 1]:
                 M[int(a) - 1] = 'x'
                 break
-    # Проверка выигрышных комбинаций
+
+
+def player_2():
+    print('ходит игрок 2')
+    b = input('Введите число от 1 до 9 : ')
+    if b not in [str(i) for i in range(1, 10)]:  # Проверка, что ввели цифру от 1 до 9
+        while b not in [str(i) for i in range(1, 10)]:
+            print('Вы ввели не то')
+            b = input('Введите число от 1 до 9 : ')
+
+    if 'x' not in M[int(b) - 1] and '0' not in M[int(b) - 1]:  # Проверка, что поле не занято
+        M[int(b) - 1] = '0'
+    else:
+        while True:
+            print('занято')
+            b = input('Введите число от 1 до 9 : ')
+            if b not in [str(i) for i in range(1, 10)]:
+                print('Вы ввели не то')
+                continue
+            if 'x' not in M[int(b) - 1] and '0' not in M[int(b) - 1]:
+                M[int(b) - 1] = '0'
+                break
+
+while True:  # Цикл для игры
+    player_1()
+    # Проверка выигрышных комбинаций для первого игрока
     if ('x' in M[0] and 'x' in M[1] and 'x' in M[2]) or ('x' in M[3] and 'x' in M[4] and 'x' in M[5]) or (
             'x' in M[6] and 'x' in M[7] and 'x' in M[8]) or ('x' in M[0] and 'x' in M[3] and 'x' in M[6]) or (
             'x' in M[1] and 'x' in M[4] and 'x' in M[7]) or ('x' in M[2] and 'x' in M[5] and 'x' in M[8]) or (
@@ -43,27 +72,8 @@ while True:  # Цикл для игры
     if '-' not in M:    # Проверка варианта 'Ничья'
         print('Ничья')
         break
-
-    print('ходит игрок 2')
-    b = input('Введите число от 1 до 9 : ')
-    if b not in [str(i) for i in range(1, 10)]:  # Проверка, что ввели цифру от 1 до 9
-        while b not in [str(i) for i in range(1, 10)]:
-            print('Вы ввели не то')
-            b = input('Введите число от 1 до 9 : ')
-    if 'x' not in M[int(b) - 1] and '0' not in M[int(b) - 1]:  # Проверка, что поле не занято
-        M[int(b) - 1] = '0'
-
-    else:
-        while True:
-            print('занято')
-            b = input('Введите число от 1 до 9 : ')
-            if b not in [str(i) for i in range(1, 10)]:
-                print('Вы ввели не то')
-                continue
-            if 'x' not in M[int(b) - 1] and '0' not in M[int(b) - 1]:
-                M[int(b) - 1] = '0'
-                break
-    # Проверка выигрышных комбинаций
+    player_2()
+    # Проверка выигрышных комбинаций для второго игрока
     if ('0' in M[0] and '0' in M[1] and '0' in M[2]) or ('0' in M[3] and '0' in M[4] and '0' in M[5]) or (
             '0' in M[6] and '0' in M[7] and '0' in M[8]) or ('0' in M[0] and '0' in M[3] and '0' in M[6]) or (
             '0' in M[1] and '0' in M[4] and '0' in M[7]) or ('0' in M[2] and '0' in M[5] and '0' in M[8]) or (
