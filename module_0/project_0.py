449391b061ee4d5bb552a064c65f93b31f52e03c
Fox27rus <rikoo1011@gmail.com>
2021-08-17 18:08:28 +1000

    Init Skillfactoryproject_1

--- module_0/project_0.py
+++ module_0/project_0.py
@@ -3,30 +3,45 @@ import numpy as np
 
 def game_core_v3(number):
     count = 1
-    predict = np.random.randint(1, 101)
-    if number // predict >= 2:
-        predict *= 2
-    elif number / predict < 1:
-        predict //= 2
+    if 0 < number <= 10:
+        predict = 5
+    elif 10 < number <= 20:
+        predict = 15
+    elif 20 < number <= 30:
+        predict = 25
+    elif 30 < number <= 40:
+        predict = 35
+    elif 40 < number <= 50:
+        predict = 45
+    elif 50 < number <= 60:
+        predict = 55
+    elif 60 < number <= 70:
+        predict = 65
+    elif 70 < number <= 80:
+        predict = 75
+    elif 80 < number <= 90:
+        predict = 85
+    elif 90 < number <= 100:
+        predict = 95
 
     while number != predict:
         count += 1
         if number > predict:
             if number % 2 == 0:
                 if predict % 2 == 0:
-                    predict += 6
+                    predict += 2
                 else:
                     predict += 1
             else:
-                predict += 7
+                predict += 3
         elif number < predict:
             if number % 2 == 0:
                 if predict % 2 == 0:
-                    predict -= 4
+                    predict -= 2
                 else:
                     predict -= 1
             else:
-                predict -= 5
+                predict -= 1
     return count  # выход из цикла, если угадали
 
 
